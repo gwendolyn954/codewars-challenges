9a7da39a44406f11e6f78b96c3b8dbe44e9fe29d
Gwendolyn Asani <gwen@indie-virtual.com>
2022-10-01 19:42:08 -0500

    Added notation for solution for 10-01-22.

--- cw-8kyu-2022-10-01/app.js
+++ cw-8kyu-2022-10-01/app.js
@@ -4,8 +4,8 @@
 // Array can contain numbers or strings. X can be either.
 
 // Return true if the array contains the value, false if not.
-
-function check(a, x) {
+//My Solution:
+function check(a, x) { 
     return(a.includes(x));
   }
 
