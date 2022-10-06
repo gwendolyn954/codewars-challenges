5aa10e2178f82a5e7a5a39fb161adbd51b8652f7
Gwendolyn Asani <gwen@indie-virtual.com>
2022-10-05 22:10:23 -0500

    Added codewars challenge for 10-05-22.

---
+++ cw-8kyu-2022-10-05/app.js
@@ -0,0 +1,14 @@
+//Sum of positive
+//DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+//Solution:
+function positiveSum(arr) {
+    let posArr = arr.filter(function(x){return x > 0});
+    let sum = 0; posArr.forEach(function (x) {sum += x;});
+    return sum;
+}  
