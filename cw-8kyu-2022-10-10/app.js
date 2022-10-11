f6c021aaa1ffcac12bc3e1603ed10905ea98261f
Gwendolyn Asani <gwen@indie-virtual.com>
2022-10-10 23:05:15 -0500

    Added codewars challenge for 10-10-22.

---
+++ cw-8kyu-2022-10-10/app.js
@@ -0,0 +1,8 @@
+//Convert a Number to a String!
+//We need a function that can transform a number (integer) into a string.
+
+//Solution:
+function numberToString(num) {
+    return num.toString();
+  }
+
