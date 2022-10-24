bb79e7cee23b38e9316296cbe1ca541e40cfe418
Gwendolyn Asani <gwen@indie-virtual.com>
2022-10-24 00:24:29 -0500

    added codewars challenge for 10-24-2022

---
+++ cw-8kyu-2022-10-24/app.js
@@ -0,0 +1,9 @@
+//MakeUpperCase
+//Write a function which converts the input string to uppercase.
+
+//My Solution:
+return str.toUpperCase();
+
+//Best Solution:
+const makeUpperCase = str => str.toUpperCase();
+
