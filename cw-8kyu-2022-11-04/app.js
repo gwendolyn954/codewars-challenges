74a00e7b4d1a543454e43e159de94411e2c8e051
Gwendolyn Asani <gwen@indie-virtual.com>
2022-11-04 23:54:44 -0600

    added codewars challenge for 11-4-2022

---
+++ cw-8kyu-2022-11-04/app.js
@@ -0,0 +1,14 @@
+// Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
+// Examples
+// convert(5) ➞ 300
+
+// convert(3) ➞ 180
+
+// convert(2) ➞ 120
+
+function convert(minutes) {
+	const seconds = minutes * 60
+	return seconds
+}
