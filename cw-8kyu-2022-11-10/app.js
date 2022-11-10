0dfd790048a480dd226f384be6cb41dff9d4956d
Gwendolyn Asani <gwen@indie-virtual.com>
2022-11-10 14:28:51 -0600

    added codewars challenge

---
+++ cw-8kyu-2022-11-10/app.js
@@ -0,0 +1,12 @@
+// How Edabit Works
+// This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+// function hello() {
+
+// }
+
+//Update the function to return a string
+
+function hello() {
+	return "hey friendddd!"
+}
