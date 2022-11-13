ebafb28c9cac6fb36826619dd94c085aafe3633c
Gwendolyn Asani <gwen@indie-virtual.com>
2022-11-13 01:26:23 -0600

    deleted solution

--- cw-8kyu-2022-10-28/app.js
+++ cw-8kyu-2022-10-28/app.js
@@ -13,7 +13,4 @@
 //Make num a string, split string into array, reverse array, return reversed array
 
 //Solution: 
-function digitize(n) {
-    return String(n).split('').map(Number).reverse()
-  }
   
