cc85126b11eb48aad2d7b76521391886a2343e6a
Gwendolyn Asani <gwen@indie-virtual.com>
2022-11-15 14:51:07 -0600

    added codewars challenge

---
+++ cw-8kyu-2022-11-15/app.js
@@ -0,0 +1,13 @@
+// Sum of Polygon Angles
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+// Examples
+// sumPolygon(3) ➞ 180
+
+// sumPolygon(4) ➞ 360
+
+// sumPolygon(6) ➞ 720
+
+function sumPolygon(n) {
+	return (n - 2) * 180
+}
