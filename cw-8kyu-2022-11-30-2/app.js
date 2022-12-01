309a7b9daa06e82ab3d288559790b4d5686e5290
Gwendolyn Asani <gwen@indie-virtual.com>
2022-11-30 18:10:38 -0600

    added update to codewars challenge for 11-30-2022

---
+++ cw-8kyu-2022-11-30-2/app.js
@@ -0,0 +1,18 @@
+//Fix the Expression
+
+// Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+// Examples
+// isSeven(4) ➞ false
+
+// isSeven(9) ➞ false
+
+// isSeven(7) ➞ true
+
+function isSeven(x) {
+	if (x === 7){
+		return true
+	}else {
+		return false
+	}
+}
