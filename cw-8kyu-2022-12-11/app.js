49807420b2aa7c05c33a03ee4316744c5455f1e6
Gwendolyn Asani <gwen@indie-virtual.com>
2022-12-11 15:53:51 -0600

    added codewars challenge 12-11-2022

---
+++ cw-8kyu-2022-12-11/app.js
@@ -0,0 +1,14 @@
+// Create a function that returns the given argument, but by using an arrow function.
+
+// An arrow function is constructed like so:
+
+// arrowFunc=(/*parameters*/)=>//code here
+// Examples
+// arrowFunc(3) ➞ 3
+
+// arrowFunc("3") ➞ "3"
+
+// arrowFunc(true) ➞ true
+
+let arrowFunc = (a) => a 
+
